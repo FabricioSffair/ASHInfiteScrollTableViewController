c5c0d384324c7c35f9b0b6cfe8aef00e102fbf0f
Gustavo Ber Tagliari <gustavo@aioria.com.br>
2017-03-08 17:26:18 -0300

    first commit

---
+++ LoadFooterView.swift
@@ -0,0 +1,46 @@
+//
+//  LoadFooterView.swift
+//  FolloU
+//
+//  Created by Gustavo B Tagliari on 22/02/17.
+//  Copyright © 2017 Aioria. All rights reserved.
+//
+
+import UIKit
+
+enum LoadFooterViewState {
+    case noMore
+    case loading
+    case undefined
+}
+
+class LoadFooterView: UITableViewHeaderFooterView {
+
+    @IBOutlet weak var actIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var indicatorLabel: UILabel!
+    
+    var noMoreResultsTitle: String = "Não há itens"
+    var loadingResultsTitle: String = "Carregando..."
+    
+    var state: LoadFooterViewState = .undefined {
+        didSet {
+            switch state {
+            case .noMore:
+                self.actIndicator?.stopAnimating()
+                self.actIndicator?.isHidden = true
+                self.indicatorLabel?.text = noMoreResultsTitle
+                break
+            case .loading:
+                self.actIndicator?.isHidden = false
+                self.actIndicator?.startAnimating()
+                self.indicatorLabel?.text = loadingResultsTitle
+                break
+            case .undefined:
+                self.actIndicator?.stopAnimating()
+                self.actIndicator?.isHidden = true
+                self.indicatorLabel?.text = nil
+                break
+            }
+        }
+    }
+}
