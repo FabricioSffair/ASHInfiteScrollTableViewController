5a984a2e9fe53cefed8c7c9b5a94e7240bca9f9e
Gustavo Bier Tagliari <gustavo@aioria.com.br>
2017-03-09 16:51:48 -0300

    creating example

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Gustavo B Tagliari on 09/03/17.
+//  Copyright © 2017 AIORIA SOFTWARE HOUSE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
